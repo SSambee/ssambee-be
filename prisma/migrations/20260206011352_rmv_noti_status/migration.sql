449981a2816e08547dedc84932ee509fcb731ce1
Lim Gyungmin <ancora.gyungmin@gmail.com>
2026-02-06 10:37:17 +0900

    feat: introduce `hasClinic` property for exams based on incomplete clinic counts

---
+++ prisma/migrations/20260206011352_rmv_noti_status/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `notification_status` on the `clinics` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "clinics" DROP COLUMN "notification_status";
