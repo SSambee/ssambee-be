0108e9e41b8b83cb2aa0c338af531b563f330033
Lim Gyungmin <ancora.gyungmin@gmail.com>
2026-02-08 17:57:03 +0900

    refactor: remove attendanceStatus field from the Assistant model

---
+++ prisma/migrations/20260208085550_remove_assistant_attendence/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `attendance_status` on the `assistants` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "assistants" DROP COLUMN "attendance_status";
