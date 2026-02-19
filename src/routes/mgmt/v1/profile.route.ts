50ba73b848a8db6b11eb80dedff3adf221416481
Lim Gyungmin <ancora.gyungmin@gmail.com>
2026-02-19 10:05:48 +0900

    feat: add routes for changing user email and password

--- src/routes/mgmt/v1/profile.route.ts
+++ src/routes/mgmt/v1/profile.route.ts
@@ -2,11 +2,19 @@ import { Router } from 'express';
 import { container } from '../../../config/container.config.js';
 import { validate } from '../../../middlewares/validate.middleware.js';
 import { updateMyProfileSchema } from '../../../validations/profile.validation.js';
+import {
+  changeMyEmailSchema,
+  changeMyPasswordSchema,
+} from '../../../validations/auth.validation.js';
 
 export const mgmtProfileRouter = Router();
 
-const { requireAuth, requireInstructorOrAssistant, profileController } =
-  container;
+const {
+  requireAuth,
+  requireInstructorOrAssistant,
+  profileController,
+  authController,
+} = container;
 
 /** 모든 라우트에 대해 강사/조교 권한 필요 */
 mgmtProfileRouter.use(requireAuth);
@@ -20,4 +28,18 @@ mgmtProfileRouter.patch(
   '/',
   validate(updateMyProfileSchema, 'body'),
   profileController.updateMyProfile,
+);
+
+/** 내 이메일 변경 */
+mgmtProfileRouter.patch(
+  '/email',
+  validate(changeMyEmailSchema, 'body'),
+  authController.changeMyEmail,
+);
+
+/** 내 비밀번호 변경 */
+mgmtProfileRouter.patch(
+  '/password',
+  validate(changeMyPasswordSchema, 'body'),
+  authController.changeMyPassword,
 );
